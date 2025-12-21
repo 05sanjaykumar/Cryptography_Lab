b6dda34303dc53877ba4d9f00a335172c4261b5f
05sanjaykumar <0508sanjaykumar@gmail.com>
2025-12-21 18:17:00 +0530

    chore: Added Hill Cipher

---
+++ Hill_Cipher_Client.java
@@ -0,0 +1,44 @@
+import java.net.*;
+import java.util.*;
+import java.io.*;
+
+
+public class Hill_Cipher_Client {
+    static int[][] inverseKey = {
+        {15, 17},
+        {20, 9}
+    };
+    static String decrypt(String text)
+    {
+        StringBuilder plain = new StringBuilder();
+
+        for(int i=0;i<text.length();i+=2)
+        {
+            int vector[] = {
+                text.charAt(i) - 'A',
+                text.charAt(i + 1) - 'A'
+            };
+            for(int j=0;j<2;j++)
+            {
+                int value = (inverseKey[j][0] * vector[0] +
+                             inverseKey[j][1] * vector[1]) % 26;
+                    plain.append((char) (value + 'A')); 
+            }
+        }
+
+        return plain.toString();
+    }
+    public static void main(String[] args) throws Exception{
+        Socket s = new Socket("localhost", 5001);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(s.getInputStream()));
+
+        String encrypted = in.readLine();
+        System.out.println("Received encrypted text: " + encrypted);
+        System.out.println("Decrypted text: " + decrypt(encrypted));
+
+            in.close();
+        s.close();
+    }
+}
---
+++ Hill_Cipher_Server.java
@@ -0,0 +1,56 @@
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+public class Hill_Cipher_Server {
+
+    static int[][] key = {
+        {3,3},
+        {2,5}
+    };
+
+    static String encrypt(String text)
+    {
+        text = text.toUpperCase().replaceAll("[^A-Z]", "");
+        if(text.length()%2!=0)
+        {
+            text+="X";
+        }
+        StringBuilder cipher = new StringBuilder();
+
+        for(int i=0;i<text.length();i+=2)
+        {
+            int[] vector = {
+                text.charAt(i)- 'A',
+                text.charAt(i+1) - 'A'
+            };
+            for(int j=0;j<2;j++)
+            {
+                int value = (key[j][0]*vector[0])+(key[j][1]*vector[1])%26;
+                cipher.append((char) (value + 'A'));
+            }   
+        }
+
+        return cipher.toString();
+
+    }
+    public static void main(String[] args)  throws Exception
+    {
+        ServerSocket ss = new ServerSocket(5001);
+        Socket s = ss.accept();
+
+        Scanner sc = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(s.getOutputStream(), true);
+
+        System.out.print("Enter plaintext: ");
+        String plaintext = sc.nextLine();
+
+        String encrypted = encrypt(plaintext);
+        System.out.println("Encrypted text: " + encrypted);
+
+        out.println(encrypted);
+
+
+
+    }
+}
